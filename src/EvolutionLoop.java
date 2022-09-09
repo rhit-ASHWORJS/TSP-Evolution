42768937b3b919f1f1a66c80620212ea72c44256
rhit-loydma <loydma@rose-hulman.edu>
2022-09-08 21:43:20 -0400

    more work on the visualizer

--- src/EvolutionLoop.java
+++ src/EvolutionLoop.java
@@ -2,57 +2,49 @@ import java.util.ArrayList;
 
 public class EvolutionLoop {
 	private static FittestRouteViewer routeViewer;
-	
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		String filename = Constants.PATH_CONF_FILE;
 		PropParser.load(filename);
 
+		int runs = Constants.RUNS;
+		for (int i = 0; i < runs; i++) {
+			if (Constants.MAP_GENERATION.equals("gui")) {
+				MapGUI mapGUI = new MapGUI();
+				mapGUI.runViewer(); //loopWithMutation is called from within here
+			} else {
+				loopWithMutation(new Map(Constants.NUM_POINTS));
+			}
+		}
+	}
+
+	public static void loopWithMutation(Map map) {
 		routeViewer = new FittestRouteViewer();
 		routeViewer.setUpViewer();
 		
-		int runs = Constants.RUNS;
-		for(int i=0; i<runs; i++)
-		{
-			loopWithMutation();
-		}
-	}
-	
-	public static void loopWithMutation()
-	{
-		Map map;
-		if(Constants.MAP_GENERATION.equals("gui")) {
-			MapGUI mapGUI = new MapGUI();
-			map = mapGUI.runViewer();
-		} else {
-			map = new Map(Constants.NUM_POINTS);
-		}
 		Generation initialGen = new Generation(Constants.POP_SIZE, map);
-		
+
 		ArrayList<Generation> generations = new ArrayList<Generation>();
 		generations.add(initialGen);
-		
-		for(int gen=0; gen<Constants.GENERATIONS; gen++)//Main loop
+
+		for (int gen = 0; gen < Constants.GENERATIONS; gen++)// Main loop
 		{
-			generations.add(GenerationFactory.newGeneration(generations.get(generations.size()-1)));
+			generations.add(GenerationFactory.newGeneration(generations.get(generations.size() - 1)));
 
 			routeViewer.updateRoute(generations.get(gen).population.get(0));
 			routeViewer.setGenIndex(gen);
 
-
-			//used to slow the run down enough to actually see the visualizer working
+			// used to slow the run down enough to actually see the visualizer working
 			try {
 				Thread.sleep(100);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println(e);
 			}
 		}
-		
-		//Console output for testing
-		for(int i=0; i<Constants.GENERATIONS; i++)
-		{
-			System.out.println("Generation "+i);
+
+		// Console output for testing
+		for (int i = 0; i < Constants.GENERATIONS; i++) {
+			System.out.println("Generation " + i);
 			generations.get(i).printGenStatistics();
 		}
 	}
--- src/FittestRouteViewer.java
+++ src/FittestRouteViewer.java
@@ -3,83 +3,80 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.util.ArrayList;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class FittestRouteViewer extends JComponent{
-    private JFrame frame;
+public class FittestRouteViewer{
+	private JFrame frame;
 	private RouteComponent component = new RouteComponent();
 	private int genIndex = 0;
 	private JLabel genNum;
 	private JLabel fitnessText;
-	
+
 	public void setUpViewer() {
 		this.frame = new JFrame();
 		this.frame.setTitle("FittestOrganismViewer");
 		this.frame.setSize(500, 550);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.frame.setLayout(new BorderLayout());
-	
+
 		this.frame.add(component, BorderLayout.CENTER);
-		
-		JPanel texts = new JPanel(); 
-		
-		
-		genNum = new JLabel(""+genIndex);
-		fitnessText = new JLabel("");
+
+		JPanel texts = new JPanel();
+
+		genNum = new JLabel("Gen: " + genIndex);
+		fitnessText = new JLabel("Distance: ");
 		texts.add(genNum);
 		texts.add(fitnessText);
-		
-		this.frame.add(texts,BorderLayout.SOUTH);
-	
+
+		this.frame.add(texts, BorderLayout.SOUTH);
+
 		this.frame.setVisible(true);
 	}
-	
+
 	public void updateRoute(Pop pop) {
 		this.component.setPop(pop);
-		genNum.setText("Gen: "+genIndex);
-		fitnessText.setText("Distance: "+pop.routeDistance());
+		genNum.setText("Gen: " + genIndex);
+		fitnessText.setText("Distance: " + pop.routeDistance());
+		this.component.repaint();
 	}
-	
+
 	public void setGenIndex(int i) {
 		this.genIndex = i;
 	}
 
-    public class RouteComponent extends JComponent{
-        private Pop pop;
+	public class RouteComponent extends JComponent {
+		private Pop pop;
 
-        
-        public RouteComponent() {
-            this.pop = new Pop(new Map());
-        }
-        
-        @Override
-        protected void paintComponent(Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
-            
+		public RouteComponent() {
+			this.pop = new Pop(new Map());
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			Graphics2D g2d = (Graphics2D) g;
+
 			int[] route = this.pop.route;
-            ArrayList<Point> points = this.pop.map.getPoints();
+			ArrayList<Point> points = this.pop.map.getPoints();
 
-            g2d.setColor(new Color(0,0,0));
-
-			for(int i = 0; i < route.length - 1; i++) {
+			for (int i = 0; i < route.length - 1; i++) {
 				Point p1 = points.get(route[i]);
-				Point p2 = points.get(route[i+1]);
+				Point p2 = points.get(route[i + 1]);
 				g2d.fillOval(p1.x - 5, p1.y - 5, 10, 10);
 				g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
 			}
-			//Finish up the drawing
-			Point p1 = points.get(route[points.size()-1]);
-			Point p2 = points.get(route[0]);
-			g2d.fillOval(p1.x - 5, p1.y - 5, 10, 10);
-			g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
-        }
-        
-        public void setPop(Pop pop) {
-            this.pop = pop;
-			this.repaint();
-        } 
-    }
+			// Finish up the drawing
+			if (points.size() > 0) {
+				Point p1 = points.get(route[points.size() - 1]);
+				Point p2 = points.get(route[0]);
+				g2d.fillOval(p1.x - 5, p1.y - 5, 10, 10);
+				g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+			}
+		}
+
+		public void setPop(Pop pop) {
+			this.pop = pop;
+		}
+	}
 }
